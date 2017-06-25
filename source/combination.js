67dc868161b4d225c7bb9166744b290cd39d73f4
Kejia <w.kejia@gmail.com>
2017-06-25 02:09:52 -0400

    changing header

--- source/combination.js
+++ source/combination.js
@@ -1,20 +1,24 @@
 
- //    This program is free software: you can redistribute it and/or modify
- //    it under the terms of the GNU Lesser General Public License as published
- //    by the Free Software Foundation, either version 3 of the License, or
- //    (at your option) any later version.
+/**
 
- //    This program is distributed in the hope that it will be useful,
- //    but WITHOUT ANY WARRANTY; without even the implied warranty of
- //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- //    GNU Lesser General Public License for more details.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
- //    You should have received a copy of the GNU Lesser General Public License
- //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
 
- //    @license The GNU Lesser General Public License, http://www.gnu.org/licenses/lgpl-3.0.txt
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
- //    @author w.kejia
+    @license The GNU Lesser General Public License, http://www.gnu.org/licenses/lgpl-3.0.txt
+
+    @author w.kejia
+
+*/
 
 /**
  * given a r-combination on [0, n), a, calculate the lexically next r-combination.
